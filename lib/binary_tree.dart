0b7bcea42c617b63b7e2ebcd9ed3f2b21671f27e
Kendal Harland <kharland@users.noreply.github.com>
2016-04-17 11:58:15 -0700

    update BinaryTreeTraversal contract

--- lib/binary_tree.dart
+++ lib/binary_tree.dart
@@ -13,7 +13,8 @@ abstract class BinaryTreeNode<T> {}
 
 /// Defines a strategy for traversing a [BinaryTree].
 abstract class BinaryTreeTraversal<T> {
-  /// Returns the successor to [node] in this traversal.
+  /// Returns the successor to [node] in this traversal or null if
+  /// no such node exists.
   BinaryTreeNode<T> next(BinaryTreeNode<T> node);
 }
 
